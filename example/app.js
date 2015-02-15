375192f7dd3a427c5e4da6413bb899612a4edc4d
mangix <maqh1988@gmail.com>
2015-02-15 19:38:43 +0800

    use

--- example/app.js
+++ example/app.js
@@ -23,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 sledge(app, require("./router.json"), path.join(__dirname, "routes"));
 
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
