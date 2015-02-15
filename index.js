7caf3341b66b3605e6ac7a97e5b6f0406f3af389
mangix <maqh1988@gmail.com>
2015-02-15 18:29:08 +0800

    parser

--- index.js
+++ index.js
@@ -2,9 +2,4 @@
  * Created by mangix on 1/31/15.
  */
 
-var parse = require("./lib/parser");
-module.exports = function (app, config, baseRoutePath) {
-    parse(config, baseRoutePath).forEach(function (router) {
-        app.use(router.url, router.controller);
-    });
-};
+module.exports = require("./lib/parser");
