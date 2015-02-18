e0cb7369ce0c0b5a61064aa3ff1e4db31918f5bd
mangix <maqh1988@gmail.com>
2015-02-18 12:21:55 +0800

    test

--- test/parser.js
+++ test/parser.js
@@ -1,3 +1,35 @@
-describe("parser" , function(){
+var expect = require("chai").expect;
+var parser = require("../index");
+var express = require("express");
+var rootPath = require("path").join(__dirname, "./actions");
 
+describe("parser", function () {
+    it("should throw an exception when action can't be found", function () {
+        expect(function () {
+            parser(express(), {
+                "some_pattern": {
+                    "action": "some_unknown_action"
+                }
+            }, rootPath);
+        }).to.throw(Error);
+
+    });
+
+
+    it("should add a handler when no sub router set", function () {
+        var app = express();
+
+        var config = {
+            "pattern_with_out_sub": {
+                "action": "./single",
+                "result": {
+                    "success": "test.jade"
+                }
+            }
+        };
+
+        parser(app, config, rootPath);
+
+
+    });
 });
